ad17b10177990ae456ba8e18a136e73522f2d74c
Maciej Zboch <maciejzboch@gmail.com>
2025-07-31 13:35:35 +0200

    feat: set up session

--- backend/App.ts
+++ backend/App.ts
@@ -1,5 +1,9 @@
 import express, {Request, Response} from 'express'
 import mongoose from 'mongoose';
+import session from 'express-session';
+import MongoStore from 'connect-mongo'
+
+
 const app = express();
 
 //MongoDB setup
@@ -7,12 +11,24 @@ const dbUrl = 'mongodb://localhost:27017/test';
 mongoose
   .connect(dbUrl)
   .then(() => {
-    console.log("Mongo connection succesful!");
+    console.log('Mongo connection succesful!');
   })
   .catch((err) => {
-    console.log("Mongo error!");
+    console.log('Mongo error!');
     console.log(err);
   });
+
+  //Session setup
+app.use(session({
+secret: 'c134cY87K43o',
+store: MongoStore.create({ 
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+crypto: {
+    secret: 'c134cY87K43o',
+  }
+})
+}));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World')
