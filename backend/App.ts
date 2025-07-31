f84825b76e4f4cbec1a79fb067a689c7891e1f22
Maciej Zboch <maciejzboch@gmail.com>
2025-07-31 13:22:25 +0200

    feat: set up MongoDB

--- backend/App.ts
+++ backend/App.ts
@@ -1,5 +1,18 @@
 import express, {Request, Response} from 'express'
+import mongoose from 'mongoose';
 const app = express();
+
+//MongoDB setup
+const dbUrl = 'mongodb://localhost:27017/test';
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log("Mongo connection succesful!");
+  })
+  .catch((err) => {
+    console.log("Mongo error!");
+    console.log(err);
+  });
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World')
